dbdd16f0782cd2a90d1b0a536f2ac18e129827e1
andrei <livanovandrej99@gmail.com>
2023-06-15 17:05:27 +0700

    fix origin

--- dist/main.js
+++ dist/main.js
@@ -7,7 +7,10 @@ const express = require("express");
 const path_1 = require("path");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: false });
-    app.enableCors({ credentials: true, origin: true });
+    app.enableCors({
+        credentials: true,
+        origin: 'https://f-storage-client.vercel.app/dashboard/auth',
+    });
     app.use('/uploads', express.static((0, path_1.join)(__dirname, '..', 'uploads')));
     const config = new swagger_1.DocumentBuilder()
         .setTitle('Облачное хранилище')
--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,12 @@ import * as express from 'express';
 import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: false });
+  const app = await NestFactory.create(AppModule);
 
-  app.enableCors({ credentials: true, origin: true });
+  app.enableCors({
+    credentials: true,
+    origin: 'https://f-storage-client.vercel.app/dashboard/auth',
+  });
 
   app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
 
